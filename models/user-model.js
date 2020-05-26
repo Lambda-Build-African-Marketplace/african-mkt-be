04c0572ab6ab5577492324568fa6a19823af3869
vanesa-tamu <vahe5495@tamu.edu>
2020-05-26 18:39:23 -0500

    functions for user model in place and will keep updating as needed

--- models/user-model.js
+++ models/user-model.js
@@ -1,10 +1,59 @@
 const db = require('../data/dbConfig.js')
 
 module.exports = {
-    find
+    findById,
+    findByFilter,
+    addUser,
+    updateUser,
+    deleteUser,
+    getUserItems
 }
 
-async function find(id){
+async function findById(id){
     const user = await db('user').where({ id }).first()
     return user
+}
+
+async function findByFilter(filter){
+    const user = await db('user').where(filter)
+    return user
+}
+
+async function addUser(user) {
+    const [id] = await db('user').insert(user, 'id');
+
+    return findById(id);
+}
+
+async function updateUser(id, changes) {
+    await db("user")
+        .where({ id })
+        .update(changes)
+
+    return findById(id);
+}
+
+function deleteUser(id) {
+    return db("user")
+        .where({ id })
+        .del();
+}
+
+async function getUserItems(id){
+    const items = await db('user_item as ui')
+        .join('user as u', 'u.id', 'ui.user_id')
+        .join('item as i', 'i.id', 'ui.item_id')
+        .join('category as c', 'c.id', 'i.category_id')
+        .where('ui.user_id', id )
+        .select(
+            'i.id',
+            'i.name',
+            'i.description',
+            'i.city',
+            'i.country',
+            'i.price',
+            'i.photo_url',
+            'c.type'
+        )
+    return items
 }
