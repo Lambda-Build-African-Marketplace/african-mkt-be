5a72a01b0fd27b7e567b9d417fb848884caa95cc
vanesa-tamu <vahe5495@tamu.edu>
2020-05-26 13:41:33 -0500

    more file structure before migrations

---
+++ knexfile.js
@@ -0,0 +1,55 @@
+// Update with your config settings.
+const dbConnection = process.env.DATABASE_URL
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './sauti.sqlite3'
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+  },
+
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/testing.sqlite3'
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './data/migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: dbConnection,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+  }
+
+};
