b1618f497eaa658fcb27419588921d3c972d2543
vanesa-tamu <vahe5495@tamu.edu>
2020-05-26 22:32:48 -0500

    Heroku fix for production migrations directory

--- knexfile.js
+++ knexfile.js
@@ -45,7 +45,7 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './data/migrations'
     },
     seeds: {
       directory: './data/seeds'
