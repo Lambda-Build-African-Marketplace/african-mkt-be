006a1a29e36e1aaa95bb6cbefa7a0395a0fb2bde
vanesa-tamu <vahe5495@tamu.edu>
2020-05-28 19:09:30 -0500

    GET for specific id on documentation

--- data/seeds/02-category.js
+++ data/seeds/02-category.js
@@ -1,8 +1,8 @@
 exports.seed = function(knex) {
   // Inserts seed entries
   return knex('category').insert([
-    {id: 1, type: 'grain'}, 
-    {id: 2, type: 'sugar'}
+    {type: 'grain'}, 
+    {type: 'sugar'}
   ]);
 };
 
